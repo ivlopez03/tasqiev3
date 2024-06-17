7df3b2e1bf7573fc5f419913a7fe496985955d33
ivlopez03 <ivlopez.dev@gmail.com>
2024-06-16 22:00:27 -0700

    Updated env variable config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// eslint-disable-next-line no-unused-vars
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
+  }
 })
