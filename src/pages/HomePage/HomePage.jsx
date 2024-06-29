e2b7f491f40b8dc40467a61a9b9df85ab33b0210
ivlopez03 <ivlopez.dev@gmail.com>
2024-06-29 02:11:05 -0700

    Updated UI - added topbar, changing colors

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,74 +1,112 @@
 import CreateWorkspaceModal from "../../components/homepage/createWorkspaceModal";
 import WorkspaceCard from "../../components/homepage/WorkspaceCard";
 import PlusIcon from "../../assets/PlusIcon";
+import HelpIcon from "../../assets/HelpIcon";
+import SettingsIcon from "../../assets/SettingsIcon";
+
 
 import { ColorRing } from "react-loader-spinner"; 
 import {  useState } from "react";
 import { useWorkspaces } from "../../context/workspaceContext/WorkspaceContext";
-
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 
 const HomePage = () => {
 
-    const { workspaces,createWorkspace,loading} = useWorkspaces();
+    const { user } = useAuth();
+
+    const { workspaces,createWorkspace,loading } = useWorkspaces();
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-
 
     const handleWorkspaceCreated = (newWorkspace) => {
         createWorkspace(newWorkspace);
         
       };
 
-    return (
-        <div className=' p-2'>
-            <div>
-                <div className="p-2">
-                    <div className="text-2xl px-3 ">Workspaces</div>
-                    <div className="py-1 px-3 w-fit">
-                        <span className="text-sm text-neutral">
-                            Manage your projects by creating workspaces
-                        </span>
-                    </div>
-                    
-                    
-                    {
-                            loading ? (
-                                <div className="flex justify-center items-center w-full h-52">
-                                    <div className="text-sm">
-                                        <div>Fetching data</div>
-                                        <div className="flex items-center justify-center">
-                                            <ColorRing 
-                                                height="40" 
-                                                width="40" 
-                                                ariaLabel="color-ring-label"
-                                                colors={['#45aeee', '#e8488a', '#fff232', '#66cc8a', '#cbcbcb']}  
-                                            />
+      function formatTodayDate() {
+        const options = { 
+            weekday: 'long', 
+            month: 'long', 
+            day: 'numeric', 
+            year: 'numeric' 
+        };
+        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(new Date());
+        return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    }
+    
+    
 
-                                        </div>
-                                    
-                                    </div>
-                                    
-                                </div>
-                            ) :   
-                             <div className="flex flex-wrap gap-4 py-2">
-                                
-                                {workspaces.map((card) => (
-                                <WorkspaceCard key={card.id} workspace={card} workspaces={workspaces} />
-                                ))}
-                        
-                                <div className="flex items-center p-3  w-[270px] min-w-[270px]">
-                                    <div className="lg:tooltip lg:tooltip-open lg:tooltip-right" data-tip="Add workspace">
-                                        <button onClick={() => setIsModalOpen(true)} className=" flex items-center justify-center border rounded-md   w-10 h-10 mr-1 text-gray-400 hover:bg-gray-100">
-                                            <PlusIcon />
-                                        </button>
-                                    </div>
-                                </div>
-                                <CreateWorkspaceModal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}  onWorkspaceCreated={handleWorkspaceCreated} />
-                            
-                            </div>
-                    }
+    return (
+        <div className="bg-[#fafafa] h-screen" >
+            <div className="relative">
+                <div className="border-b p-4 flex items-center relative justify-between bg-white">
+                    <div className="text-lg font-light  ">Dashboard</div>
+                    <div className="flex gap-4 px-10">
+                        <div><HelpIcon/></div>
+                        <div><SettingsIcon/></div>
+                    </div>
                 </div>
+                
+                    
+                <div className="p-5"> 
+                    <div className="pb-5">
+                        <span className="text-lg font-semibold">Hello, {user.name} !</span>
+                        <div>
+                            <span className="text-[#909090] font-light">{formatTodayDate()}</span>
+                        </div>
+                    </div>                   
+                    <div>
+                        <div className="p-2 w-[600px] max-h-[370px] border rounded-md shadow-sm  bg-white">
+                            <div className="flex items-centertext-md px-3 relative">
+                                <div className="flex items-center gap-1">Workspaces <span className="text-white bg-black text-[9px] px-2 py-1 rounded-xl">{workspaces.length}</span></div>
+                                <Link to={'/workspaces'} className="absolute right-3">
+                                    <span className=" underline text-sm">See all</span>
+                                </Link>    
+                            </div>
+                            {
+                                    loading ? (
+                                        <div className="flex justify-center items-center w-full h-52">
+                                            <div className="text-sm">
+                                                <div>Fetching data</div>
+                                                <div className="flex items-center justify-center">
+                                                    <ColorRing 
+                                                        height="40" 
+                                                        width="40" 
+                                                        ariaLabel="color-ring-label"
+                                                        colors={['#45aeee', '#e8488a', '#fff232', '#66cc8a', '#cbcbcb']}  
+                                                    />
+
+                                                </div>
+                                            
+                                            </div>
+                                            
+                                        </div>
+                                    ) :   
+                                    <div className="flex flex-wrap gap-4 py-2 ">
+                                        
+                                        {workspaces.map((card) => (
+                                        <WorkspaceCard key={card.id} workspace={card} workspaces={workspaces} />
+                                        ))}
+
+                                        {
+                                            workspaces.length < 8 &&
+                                                <div className="flex items-center p-3  w-[270px] min-w-[270px]">
+                                                    <div className="lg:tooltip lg:tooltip-open lg:tooltip-right" data-tip="Add workspace">
+                                                        <button onClick={() => setIsModalOpen(true)} className=" flex items-center justify-center border rounded-md   w-10 h-10 mr-1 text-gray-400 hover:bg-gray-100">
+                                                            <PlusIcon />
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            }
+                                        <CreateWorkspaceModal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}  onWorkspaceCreated={handleWorkspaceCreated} />
+                                    </div>
+                            }
+                        </div>
+                        
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
